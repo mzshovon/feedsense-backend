17d3a4d52d80fef212367080c806b52edff6d144
Zaman Shovon <zaman.monir20188@gmail.com>
2024-11-30 13:06:53 +0600

    Api route is setup

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/', function(){
+    return response(
+        [
+            "status" => Response::HTTP_OK,
+            "message" => "Application is up"
+        ]
+    );
+});
